fc9517c92bfa36d1867d4c17cac1b938c900ce44
avaishnav <avaishnav@grepruby.com>
2018-11-26 19:01:18 +0530

    WIP Add ReputationSDK class

--- lib/contracts/createPassport.ts
+++ lib/contracts/createPassport.ts
@@ -2,18 +2,26 @@ import createInstance from '../providers/createInstance';
 import performAsync from '../providers/performAsync';
 import loader from '../providers/loader';
 
-const createPassport = async function (abi: any, atAddress: string): Promise<string> {
-  const contract = createInstance(abi, atAddress);
-  let trxHash: any;
-  try {
-    trxHash = await performAsync(contract.createPassport.bind(null));
-  } catch (err) {
-    return err;
-  }
-  const result: any = await loader(trxHash);
-  let passport: string = result.logs[0].topics[1];
-  passport = '0x' + passport.slice(26);
-  return passport;
-};
+export class CreatePassport {
+  contract: any;
 
-export default createPassport;
+  constructor(abi: any, atAddress?: string) {
+    this.contract = createInstance(abi, atAddress);
+  }
+
+  async generateEmptyPassport(): Promise<string> {
+    let trxHash: any;
+    try {
+      trxHash = await performAsync(this.contract.createPassport.bind(null));
+    } catch (err) {
+      return err;
+    }
+
+    const result: any = await loader(trxHash);
+    let passport: string = result.logs[0].topics[1];
+    passport = '0x' + passport.slice(26);
+    return passport;    
+  }
+}
+
+export default CreatePassport;
