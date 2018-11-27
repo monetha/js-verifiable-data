2c7163ea06b5a89e31d6c7ba7b5bd469dbe81cd6
Shanu <shanu.goyanka@monetha.io>
2018-11-27 20:47:32 +0530

    Changed readpassport to class from static function

--- lib/contracts/readPassportFacts.ts
+++ lib/contracts/readPassportFacts.ts
@@ -1,43 +1,129 @@
 import createInstance from '../providers/createInstance';
 import performAsync from '../providers/performAsync';
 
+
+interface IReturnString {
+  "res": string;
+  "err": any;
+}
+interface IReturnNumber {
+  "res": Number;
+  "err": any;
+}
+interface IReturnBool {
+  "res": Boolean;
+  "err": any;
+}
+interface IReturnBytes {
+  "res": ByteLengthChunk;
+  "err": any;
+}
 const globalWindow: any = window;
-const readPassportFacts = async function (abi: any, passportAddress: string, factProviderAddress: string, dataType: string, key: string) {
-  const contract = createInstance(abi, passportAddress);
-  key = globalWindow.web3.fromAscii(key);
-  let selectedMethod: any;
-  switch (dataType) {
-    case "string": {
-      selectedMethod = contract.getString.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "bytes": {
-      selectedMethod = contract.getBytes.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "address": {
-      selectedMethod = contract.getAddress.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "uint": {
-      selectedMethod = contract.getUint.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "int": {
-      selectedMethod = contract.getInt.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "bool": {
-      selectedMethod = contract.getBool.bind(null, factProviderAddress, key);
-    }
-      break;
-    case "txdata": {
-      selectedMethod = contract.getTxDataBlockNumber.bind(null, factProviderAddress, key);
-    }
-      break;
+
+export class readPassportFacts {
+  contract: any;
+
+  constructor(abi: any, atAddress?: string) {
+    this.contract = createInstance(abi, atAddress);
   }
-  const result: any = await performAsync(selectedMethod);
-  return result;
+
+  async getString(factProviderAddress: string, key: string): Promise<IReturnString> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnString = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getString.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getBytes(factProviderAddress: string, key: string): Promise<IReturnBytes> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnBytes = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getBytes.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getAddress(factProviderAddress: string, key: string): Promise<IReturnString> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnString = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getAddress.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getUint(factProviderAddress: string, key: string): Promise<IReturnNumber> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnNumber = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getUint.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getInt(factProviderAddress: string, key: string): Promise<IReturnNumber> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnNumber = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getInt.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getBool(factProviderAddress: string, key: string): Promise<IReturnBool> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnBool = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getBool.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
+
+  async getTxDataBlockNumber(factProviderAddress: string, key: string): Promise<IReturnBytes> {
+    key = globalWindow.web3.fromAscii(key);
+    let result: IReturnBytes = {"res": null, "err": null};
+    let txResult: any;
+    try {
+      txResult = await performAsync(this.contract.getTxDataBlockNumber.bind(null, factProviderAddress, key));
+    } catch (err) {
+      result.err = err;
+      return result;
+    }
+    result.res = txResult[1];
+    return result;
+  }
 }
 
 export default readPassportFacts;
