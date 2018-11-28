f067b41ae35a4f6c8077409084b2d0565f4afec1
avaishnav <avaishnav@grepruby.com>
2018-11-28 15:16:00 +0530

    updated mechanics to collect filteredEvents

--- lib/contracts/getPassportLists.ts
+++ lib/contracts/getPassportLists.ts
@@ -10,19 +10,14 @@ interface IFilteredEvents {
 //method to fetch all the passport created by a particular passportFactory address
 const getPassportLists = async function (factoryAddress: string): Promise<Array<IFilteredEvents>> {
 
-  let events = await fetchEvents(factoryAddress);
+  const events = await fetchEvents(factoryAddress);
   let filteredEvents: Array<IFilteredEvents>;
-  filteredEvents = (events as Array<any>).map((event) => {
-
-    let filteredEvent: IFilteredEvents;
-
-    filteredEvent.blockNumber = event.blockNumber;
-    filteredEvent.blockHash = event.blockHash;
-    filteredEvent.passportAddress = '0x' + event.topics[1].slice(26);
-    filteredEvent.ownerAddress = '0x' + event.topics[2].slice(26);
-  
-    return filteredEvent;
-  });
+  filteredEvents = (events as Array<any>).map((event) => ({
+    blockNumber: event.blockNumber,
+    blockHash: event.blockHash,
+    passportAddress: '0x' + event.topics[1].slice(26),
+    ownerAddress: '0x' + event.topics[2].slice(26),
+  }));
 
   return filteredEvents;
 }
--- lib/contracts/readPassportHistory.ts
+++ lib/contracts/readPassportHistory.ts
@@ -12,17 +12,15 @@ const globalWindow: any = window;
 //method to fetch all the events(history) of a particular passportFactory address
 const readPassportHistory = async function (factoryAddress: string): Promise<Array<IFilteredFact>> {
 
-  let facts  = await fetchEvents(factoryAddress);
+  const facts  = await fetchEvents(factoryAddress);
   let filteredFacts: Array<IFilteredFact>;
-  filteredFacts = (facts as Array<any>).map((fact) => {
-    let filteredFact: IFilteredFact;
-    filteredFact.blockNumber = fact.blockNumber;
-    filteredFact.transactionHash = fact.transactionHash;
-    filteredFact.factProviderAddress = '0x' + fact.topics[1].slice(26);
-    filteredFact.key = globalWindow.web3.toAscii(fact.topics[2].slice(0,23));
-  
-    return filteredFact;
-  });
+  filteredFacts = (facts as Array<any>).map(fact => ({
+    blockNumber: fact.blockNumber,
+    transactionHash: fact.transactionHash,
+    factProviderAddress: '0x' + fact.topics[1].slice(26),
+    key: globalWindow.web3.toAscii(fact.topics[2].slice(0,23)),
+  }));
+
   return filteredFacts;
 }
 
