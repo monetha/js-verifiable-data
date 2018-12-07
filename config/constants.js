9b9be103951c22aeb091d88cae601dace3054cd4
avaishnav <avaishnav@grepruby.com>
2018-12-07 19:47:27 +0530

    Using jsonrpc to fetch events

---
+++ config/constants.js
@@ -0,0 +1,2 @@
+
+export const INFURA_END_POINT = 'https://ropsten.infura.io/v3/1f09dda6cce44da68213cacb1ea9bb90';
--- lib/providers/fetchEvents.ts
+++ lib/providers/fetchEvents.ts
@@ -1,24 +1,23 @@
+import axios from 'axios';
+import { INFURA_END_POINT } from '../../config/constants';
+
 const fetchEvents = async function (address, web4) {
-
-  const filter = web4.eth.filter({
-    fromBlock: 0,
-    toBlock: 'latest',
-    address: address
-  });
-
-  const getAsync = () => new Promise((resolve, reject) => {
-    filter.get((err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
+  const requestData = {
+    id:1,
+    jsonrpc: "2.0",
+    method: "eth_getLogs",
+    params: [
+      {
+        fromBlock: "0x0",
+        toBlock: "0x6c6174657374",
+        address,
       }
-    })
-  })
+    ],
+  };
 
-  const events = await getAsync();
+  const response = await axios.post(INFURA_END_POINT, requestData);
  
-  return events;
+  return response.data.result;
 };
 
 export default fetchEvents;
