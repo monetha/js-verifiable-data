421c9a3c0d0fa64a447b4a0cec80f6d3f65b3494
Martynas Adomaitis <martynas.adomaitis@monetha.io>
2019-01-18 17:19:49 +0200

    Added comment

--- lib/passport/FactReader.ts
+++ lib/passport/FactReader.ts
@@ -107,6 +107,7 @@ export class FactReader {
    *
    * @param factProviderAddress fact provider to read fact for
    * @param key fact key
+   * @param ipfs IPFS client
    */
   public async getIPFSData(factProviderAddress: Address, key: string, ipfs: IIPFSClient): Promise<any> {
     const hash = await this.get('getIPFSHash', factProviderAddress, key);
